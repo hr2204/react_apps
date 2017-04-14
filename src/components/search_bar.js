9c0d8b75c7f472231f3c12728481d1b64619fc9f
rhe <ren.he@afficienta.com>
2017-04-14 09:40:18 -0700

    test youtube api

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-class SearchBar extends React.Component {
+class SearchBar extends Component {
+  constructor(props){
+    super(props)
 
+    this.state = { term: ""};
+  }
 
   render(){
-    return <input />;
+    return <input onChange={event => this.setState({ term: event.target.value})}/>;
   }
+
+
+
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import ReactDom from 'react-dom'
-import SearchBar from './components/search_bar'
+import ReactDom from 'react-dom';
+import SearchBar from './components/search_bar';
+
+import YTSearch from 'youtube-api-search';
 //creat a new component
 // produce html
 const API_KEY = 'AIzaSyD2F84fATr5tf7YDbGv-IAvatNWVK-mJRU';
+
+YTSearch({key:API_KEY,term:'surfboards'},function(data){
+  console.info(data)
+})
 
 const App = () => {
   return (
