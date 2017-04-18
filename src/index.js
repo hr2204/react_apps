def8ee6779389008f35c261a73ad3056709692f2
hr2204 <hr2204@hotmail.com>
2017-04-18 14:58:00 -0700

    chapter 2 lec 29

--- src/index.js
+++ src/index.js
@@ -2,29 +2,42 @@ import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
 import YTSearch from 'youtube-api-search';
+
 //creat a new component
 // produce html
 const API_KEY = 'AIzaSyD2F84fATr5tf7YDbGv-IAvatNWVK-mJRU';
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      videos:[]
+        videos:[],
+        selectedVideo: null
     };
 
-    YTSearch({key:API_KEY,term:'surfboards'}, (videos)=>{
-      this.setState({videos});
+    YTSearch({key:API_KEY,term:'surfboards'}, (videos) => {
+      this.setState({
+          videos: videos,
+          selectedVideo: videos[0]
+      });
     });
   }
 
   render(){
     return (
       <div>
-        <SearchBar />
-        <VideoList videos={this.state.videos} />
+          <div>
+              <SearchBar />
+          </div>
+        <div>
+          <VideoDetail video={this.state.selectedVideo}/>
+        </div>
+        <VideoList
+            onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+            videos={this.state.videos} />
       </div>
     );
   }
