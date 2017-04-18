f456ce586b53ad432afe9994ab19b0fa3b573882
hr2204 <hr2204@hotmail.com>
2017-04-18 09:59:15 -0700

    update

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const VideoListItem = ({video}) => {
   // const video = props.video;
-  console.log(video,'111')
+  console.log(video,'1111')
   return <li>Video</li  >
 }
 
